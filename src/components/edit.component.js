07e02364b0e1c018c66bda0a95ad576a904f89b1
Senecacherry <42197371+Senecacherry@users.noreply.github.com>
2019-03-09 11:15:58 -0700

    components

---
+++ src/components/edit.component.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+
+export default class Edit extends Component {
+
+    render() {
+        
+        return (
+
+            <div>
+
+                <p> Edit your symptoms here!</p>
+
+            </div>
+        )
+    }
+}
